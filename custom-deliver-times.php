0eef8df8c641639cb3df39b7388184c9d0cc4dbb
Richard Danklof <r.danklof@medpets.nl>
2021-02-23 22:24:12 +0100

    Exclude files when archiving

--- custom-deliver-times.php
+++ custom-deliver-times.php
@@ -58,3 +58,33 @@ function custom_woocommerce_email_order_meta_fields( $fields, $sent_to_admin, $o
     ];
     return $fields;
 }
+
+//
+//function dbi_add_settings_page() {
+//    add_options_page( 'Custom Deliver Times', 'Custom Deliver Times', 'manage_options', 'custom-deliver-times',
+//    'dbi_render_plugin_settings_page' );
+//}
+//add_action( 'admin_menu', 'dbi_add_settings_page' );
+//
+//
+//function dbi_render_plugin_settings_page() {
+//    ?>
+<!--    <h2>Custom Deliver Times</h2>-->
+<!--    <form action="options.php" method="post">-->
+<!--        --><?php
+//        settings_fields( 'dbi_example_plugin_options' );
+//        do_settings_sections( 'dbi_example_plugin' ); ?>
+<!--        <input name="submit" class="button button-primary" type="submit" value="--><?php //esc_attr_e( 'Save' ); ?><!--" />-->
+<!--    </form>-->
+<!--    --><?php
+//}
+//
+//function dbi_register_settings() {
+//    register_setting( 'dbi_example_plugin_options', 'dbi_example_plugin_options', 'dbi_example_plugin_options_validate' );
+//    add_settings_section( 'api_settings', 'API Settings', 'dbi_plugin_section_text', 'dbi_example_plugin' );
+//
+//    add_settings_field( 'dbi_plugin_setting_api_key', 'API Key', 'dbi_plugin_setting_api_key', 'dbi_example_plugin', 'api_settings' );
+//    add_settings_field( 'dbi_plugin_setting_results_limit', 'Results Limit', 'dbi_plugin_setting_results_limit', 'dbi_example_plugin', 'api_settings' );
+//    add_settings_field( 'dbi_plugin_setting_start_date', 'Start Date', 'dbi_plugin_setting_start_date', 'dbi_example_plugin', 'api_settings' );
+//}
+//add_action( 'admin_init', 'dbi_register_settings' );
